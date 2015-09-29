b19d6ebac2755ea241152a4f1416ae58509d15bf
Johnathan Chei <j.chei175@gmail.com>
2015-09-29 01:29:51 -0400

    started on kathy's site

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Kathy-Site
+##Requests:
+- White BG and Black Text
+- Contant Info (no ‘booking’ feature)
+- Include affiliation with Melange NYC
+- Testamonials
---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var ejs = require('ejs');
+var app = express();
+app.use(express.static('public'));
+app.set('view_engine','ejs');
+
+app.get("/",function(req,res){
+	res.render('index.ejs');
+})
+
+app.listen(3000,function(){
+	console.log("Kathy's site is live");
+})
