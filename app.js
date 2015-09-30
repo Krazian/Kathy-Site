cf99b15e19f542c6ebc0ee30e11d93b788f483a1
Johnathan Chei <j.chei175@gmail.com>
2015-09-30 13:39:05 -0400

    fine tuned img carousel

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 app.use(express.static('public'));
 app.set('view_engine','ejs');
 
+//single page only need
 app.get("/",function(req,res){
 	res.render('index.ejs');
 })
--- public/scripts.js
+++ public/scripts.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
-	for (var i = 1; i < 25; i++){
+	//adds all pictures to the DOM
+	for (var i = 1; i < 28; i++){
 		var img = $('<img>');
-		img.attr('src', '/../images/'+i+'.jpg');
-		img.appendTo('.images');
+		img.attr('src', '/../images/'+i+'a.jpg');
+		img.addClass('image');
+		img.appendTo('.images-preview');
 	}
 
-  $('.images').slick({
-  	dots: true,
+	//image carousel
+  $('.images-preview').slick({
+    slidesToShow: 5,
+    slidesToScroll: 4,
   	infinite: true,
     autoplay: true,
     autoPlaySpeed: 200,
+  	dots: true,
     arrows: true,
-    slidesToShow: 5,
-    slidesToScroll: 4,
-    touchToMove: true
+    pauseOnHover: true,
+    adaptiveHeight: true
   });
 
+  //click events for modals
   $(".about").click(function () {
     $('#aboutModal').foundation('reveal', 'open');
-});
+	});
   $(".testimonials").click(function () {
     $('#testimonialsModal').foundation('reveal', 'open');
-});
+	});
   $(".contact").click(function () {
     $('#contactModal').foundation('reveal', 'open');
-});
+	});
 });
