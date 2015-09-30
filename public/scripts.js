faa5057f6016b6f16adaf3706f9901debfbcd802
Johnathan Chei <j.chei175@gmail.com>
2015-09-30 17:54:14 -0400

    added 2 bars to separate images, note: there should probably be some words

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,50 @@
 $(document).ready(function(){
 	//adds all pictures to the DOM
-	for (var i = 1; i < 28; i++){
+	for (var i = 1; i <= 17; i++){
 		var img = $('<img>');
-		img.attr('src', '/../images/'+i+'a.jpg');
+		img.attr('src', '/../images/headshots/'+i+'.jpg');
 		img.addClass('image');
-		img.appendTo('.images-preview');
+		img.appendTo('.headshots');
+	}
+	for (var i = 1; i <= 4; i++){
+		var img = $('<img>');
+		img.attr('src', '/../images/weddingprom/'+i+'.jpg');
+		img.addClass('image');
+		img.appendTo('.wedding-and-prom');
+	}
+	for (var i = 1; i <= 5; i++){
+		var img = $('<img>');
+		img.attr('src', '/../images/beforeafter/'+i+'.jpg');
+		img.addClass('image');
+		img.appendTo('.before-and-after');
 	}
 
 	//image carousel
-  $('.images-preview').slick({
+  $('.headshots').slick({
+    slidesToShow: 5,
+    slidesToScroll: 4,
+  	infinite: true,
+    autoplay: true,
+    autoPlaySpeed: 3000,
+  	dots: true,
+    arrows: true,
+    pauseOnHover: true
+  });
+
+  //image carousel
+  $('.wedding-and-prom').slick({
+    slidesToShow: 5,
+    slidesToScroll: 4,
+  	infinite: true,
+    autoplay: true,
+    autoPlaySpeed: 3000,
+  	dots: true,
+    arrows: true,
+    pauseOnHover: true
+  });
+
+  //image carousel
+  $('.before-and-after').slick({
     slidesToShow: 5,
     slidesToScroll: 4,
   	infinite: true,
